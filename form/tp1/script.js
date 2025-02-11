94010f44c3d2cbbee095a9950d85dc5d441f6da8
douvewane <douvewane85@gmail.com>
2025-02-11 05:49:30 +0000

    Validation champ text + application des classes erreurs

--- form/tp1/script.js
+++ form/tp1/script.js
@@ -6,21 +6,61 @@ const professeurs = [
 ];
 //Recuperer le formualire
 const formProf=document.getElementById("form-prof");
-//Activer evenement 
+const inputNom=document.querySelector("#nom");
+const errorNom=inputNom.nextElementSibling
+const inputPrenom=document.querySelector("#prenom");
+const errorPrenom=document.querySelector(`#${inputPrenom.id}Error`);
+//Soummission Formulaire
 formProf.addEventListener("submit", function(event){
     event.preventDefault();
-    const inputNom=document.querySelector("#nom");
-
-    inputNom.nextElementSibling.textContent="Ce champ est obligatoire"
-    
-    const inputPrenom=document.querySelector("#prenom");
-
-     addProfesseur({nom:inputNom.value,prenom:inputPrenom.value})
+    //Gestion Error sur le nom
+   
+     if (inputNom.value=="") {
+         errorNom.textContent="Ce champ est obligatoire"
+          //application des classes error
+          inputNom.classList.add("is-invalid")
+          errorNom.classList.add("invalid-feedback")
+         return;
+     }
+          inputNom.classList.remove("is-invalid")
+          errorNom.classList.remove("invalid-feedback")
+          inputNom.classList.add("is-valid")
+          errorNom.classList.add("valid-feedback")
+       //Gestion Error sur le prenom
       
-      formProf.reset(); 
-    
-     
+     if (inputPrenom.value=="") {
+         errorPrenom.textContent="Ce champ est obligatoire"
+          //application des classes error
+          inputPrenom.classList.add("is-invalid")
+          errorPrenom.classList.add("invalid-feedback")
+        
+         return;
+     }
+     inputPrenom.classList.remove("is-invalid")
+     errorPrenom.classList.remove("invalid-feedback")
+     inputPrenom.classList.add("is-valid")
+     errorPrenom.classList.add("valid-feedback")
+   
+     //Ajout dans la liste 
+    addProfesseur({nom:inputNom.value,prenom:inputPrenom.value})
+    formProf.reset();    
 })
+
+inputNom.addEventListener("focus",() => {
+    if(inputNom.classList.contains("is-invalid") ){
+      
+        inputNom.classList.remove("is-invalid")
+        errorNom.classList.remove("invalid-feedback")
+        errorNom.textContent=""
+    }
+
+    if(inputNom.classList.contains("is-valid")){
+        inputNom.classList.remove("is-valid")
+        errorNom.classList.remove("valid-feedback")
+    }
+
+})
+
 
 function addProfesseur(professeur){
     professeurs.push(professeur);
